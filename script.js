3c2f75e3c5e4e4426be40b57e771f94a2eeb3efc
omasanetsano <etsanoomasanjuwa@gmail.com>
2024-03-07 12:42:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById('pad1').addEventListener('click', function() {
+    var audio = document.getElementById('sound1');
+    audio.play();
+});
+
+document.getElementById('pad2').addEventListener('click', function() {
+    var audio = document.getElementById('sound2');
+    audio.play();
+});
+
+document.getElementById('pad3').addEventListener('click', function() {
+    var audio = document.getElementById('sound3');
+    audio.play();
+});
+
+
+document.getElementById('pad4').addEventListener('click', function() {
+    var audio = document.getElementById('sound4');
+    audio.play();
+});
+
+
+document.getElementById('pad5').addEventListener('click', function() {
+    var audio = document.getElementById('sound5');
+    audio.play();
+});
+
+
+document.getElementById('pad6').addEventListener('click', function() {
+    var audio = document.getElementById('sound6');
+    audio.play();
+});
